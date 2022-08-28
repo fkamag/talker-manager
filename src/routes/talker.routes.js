32a45014fda7fe05c566257df9386afa12370210
fkamag <fabiokamagian@gmail.com>
2022-08-28 17:33:43 -0300

    requisito 2

--- src/routes/talker.routes.js
+++ src/routes/talker.routes.js
@@ -3,9 +3,27 @@ const talkerDb = require('../data/talker.db');
 
 const router = express.Router();
 
-router.get('/talker', async (req, res) => {
+const talkers = async () => {
   const result = await talkerDb.read();
+  return result;
+};
+
+router.get('/talker', async (req, res) => {
+  const result = await talkers();
   res.status(200).json(result);
+});
+
+router.get('/talker/:id', async (req, res, next) => {
+  const { id } = req.params;
+  console.log(id);
+  const result = await talkers();
+  const talker = result.find((item) => item.id === Number(id));
+
+  if (!talker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  return res.status(200).json(talker);
 });
 
 module.exports = router;
