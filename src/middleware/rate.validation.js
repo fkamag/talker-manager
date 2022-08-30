5d0920b2ed0c1885e292f2d7cf3c2945505cfc0d
fkamag <fabiokamagian@gmail.com>
2022-08-29 21:09:40 -0300

    finalizando requisito 6

--- src/middleware/rate.validation.js
+++ src/middleware/rate.validation.js
@@ -1,6 +1,6 @@
 const rateValidation = (req, res, next) => {
   const { rate } = req.body.talk;
-  if (!rate) {
+  if (rate === undefined) {
     return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
   }
   if (!Number.isInteger(rate) || rate < 1 || rate > 5) {
